8e4b1105ba9598620e32ac26e66e312f173b81a6
Igor Artamonov <igor@artamonov.ru>
2011-11-04 18:36:04 +0700

    v0.3

--- SpringSecurityFacebookGrailsPlugin.groovy
+++ SpringSecurityFacebookGrailsPlugin.groovy
@@ -22,7 +22,7 @@ import com.the6hours.grails.springsecurity.facebook.FacebookAuthCookieLogoutHand
 
 class SpringSecurityFacebookGrailsPlugin {
 
-   String version = '0.2.4'
+   String version = '0.3'
    String grailsVersion = '1.3.7 > *'
    Map dependsOn = ['springSecurityCore': '1.0 > *']
 
