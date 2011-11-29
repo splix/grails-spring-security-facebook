947d171a862f547a80772059248c0bae3e4eca47
Igor Artamonov <igor@artamonov.ru>
2011-11-30 00:00:44 +0700

    facebook:connect tag

--- grails-app/taglib/com/the6hours/grails/springsecurity/facebook/FacebookAuthTagLib.groovy
+++ grails-app/taglib/com/the6hours/grails/springsecurity/facebook/FacebookAuthTagLib.groovy
@@ -13,20 +13,65 @@ class FacebookAuthTagLib {
 
 	static namespace = 'facebookAuth'
 
+    static final String MARKER = 'com.the6hours.grails.springsecurity.facebook.FacebookAuthTagLib#init'
+
 	/** Dependency injection for springSecurityService. */
 	def springSecurityService
 
 	def connect = { attrs, body ->
         def conf = SpringSecurityUtils.securityConfig.facebook
-        String lang = conf.language
-        def appId = conf.appId
-        def buttonText = conf.button.text
 
-        out << '<div id="fb-root"></div>'
-        out << '<script src="http://connect.facebook.net/$lang/all.js"></script>'
-        out << '<script> FB.init({ appId:"$appId", cookie:true, status:true, xfbml:true});</script>'
+        Boolean init = request.getAttribute(MARKER)
+        if (attrs.requirejs != 'false' && (init == null || !init)) {
+            String lang = conf.language
+            def appId = conf.appId
+            out << '<div id="fb-root"></div>\n'
 
-        out << '<fb:login-button>$buttonText</fb:login-button>'
+            out << '<script>\n'
+
+            out << "window.fbAsyncInit = function() {\n"
+            out << "  FB.init({\n"
+            out << "    appId  : '${appId}',\n"
+            out << "    status : true,\n"
+            out << "    cookie : true,\n"
+            out << "    xfbml  : true\n"
+            out << "  });\n"
+            out << "};\n"
+
+            out << '(function(d){'
+            out << "var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}"
+            out << "js = d.createElement('script'); js.id = id; js.async = true;"
+            out << "js.src = \"//connect.facebook.net/${lang}/all.js\";"
+            out << "d.getElementsByTagName('head')[0].appendChild(js);"
+            out << '}(document));\n'
+
+            out << '</script>\n'
+
+            request.setAttribute(MARKER, true)
+        }
+
+
+        String buttonText = conf.button.text
+        if (attrs.text) {
+            buttonText = attrs.text
+        }
+
+        List permissions = []
+        if (attrs.permissions) {
+            if (attrs.permissions instanceof Collection) {
+                permissions = attrs.permissions.findAll {
+                    it != null
+                }.collect {
+                    it.toString().trim()
+                }.findAll {
+                    it.length() > 0
+                }
+            } else {
+                permissions = attrs.permissions.toString().split(',').collect { it.trim() }
+            }
+        }
+
+        out << "<div class=\"fb-login-button\" data-perms=\"${permissions.join(', ')}\">$buttonText</div>"
     }
 
 }
