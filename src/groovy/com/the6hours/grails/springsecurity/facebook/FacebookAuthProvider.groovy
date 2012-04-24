111a25e5604a26b9c312b2e0728790f7cd144810
Igor Artamonov <igor@artamonov.ru>
2012-04-24 17:58:03 +0700

    code formatting; use principal for getRoles

--- src/groovy/com/the6hours/grails/springsecurity/facebook/FacebookAuthProvider.groovy
+++ src/groovy/com/the6hours/grails/springsecurity/facebook/FacebookAuthProvider.groovy
@@ -4,7 +4,7 @@ import org.springframework.security.core.Authentication
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.GrantedAuthorityImpl
+
 import org.springframework.security.core.userdetails.User
 import org.apache.log4j.Logger
 
@@ -12,26 +12,26 @@ public class FacebookAuthProvider implements AuthenticationProvider {
 
     private static def log = Logger.getLogger(this)
 
-	FacebookAuthDao facebookAuthDao
+    FacebookAuthDao facebookAuthDao
     FacebookAuthUtils facebookAuthUtils
 
-	boolean createNew = true 
+    boolean createNew = true
 
-	public Authentication authenticate(Authentication authentication) {
-		FacebookAuthToken token = authentication
-		
-		def user = facebookAuthDao.findUser(token.uid as Long)
-		
-		if (user == null) {
-			//log.debug "New person $token.uid"
-			if (createNew) {
-				log.info "Create new facebook user with uid $token.uid"
+    public Authentication authenticate(Authentication authentication) {
+        FacebookAuthToken token = authentication
+
+        def user = facebookAuthDao.findUser(token.uid as Long)
+
+        if (user == null) {
+            //log.debug "New person $token.uid"
+            if (createNew) {
+                log.info "Create new facebook user with uid $token.uid"
                 token.accessToken = facebookAuthUtils.getAccessToken(token.code)
-				user = facebookAuthDao.create(token)
-			} else {
+                user = facebookAuthDao.create(token)
+            } else {
                 log.error "User $token.uid not exists - not authenticated"
             }
-		}
+        }
         if (user != null) {
             UserDetails userDetails = createUserDetails(user, token.code)
 
@@ -41,17 +41,17 @@ public class FacebookAuthProvider implements AuthenticationProvider {
         } else {
             token.authenticated = false
         }
-		return token
-	}
+        return token
+    }
 
-	public boolean supports(Class<? extends Object> authentication) {
-		return FacebookAuthToken.isAssignableFrom(authentication);
-	}
+    public boolean supports(Class<? extends Object> authentication) {
+        return FacebookAuthToken.isAssignableFrom(authentication);
+    }
 	
-   protected UserDetails createUserDetails(Object user, String secret) {
-	   Collection<GrantedAuthority> roles = facebookAuthDao.getRoles(user)
-	   new User(user.uid.toString(), secret, true,
-			   true, true, true, roles)
-   }
+    protected UserDetails createUserDetails(Object facebookUser, String secret) {
+        Collection<GrantedAuthority> roles = facebookAuthDao.getRoles(facebookAuthDao.getPrincipal(facebookUser))
+        new User(facebookUser.uid.toString(), secret, true,
+             true, true, true, roles)
+    }
 
 }
