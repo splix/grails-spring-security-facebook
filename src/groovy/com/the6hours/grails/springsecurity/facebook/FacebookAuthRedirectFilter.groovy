09ceb9ae13e11e0972c78afa0790600138570613
fernando <fernando@fernando-laptop.(none)>
2012-12-30 18:18:16 -0300

    Fix redirect_uri missing "/" separation

--- src/groovy/com/the6hours/grails/springsecurity/facebook/FacebookAuthRedirectFilter.groovy
+++ src/groovy/com/the6hours/grails/springsecurity/facebook/FacebookAuthRedirectFilter.groovy
@@ -63,7 +63,7 @@ class FacebookAuthRedirectFilter extends AbstractAuthenticationProcessingFilter 
 
     String getAbsoluteRedirectUrl() {
         String path = getFilterProcessesUrl()
-        if (path.startsWith('/')) {
+        if (linkGenerator.serverBaseURL.endsWith('/') && path.startsWith('/')) {
             path = path.substring(1)
         }
         linkGenerator.link(uri: path, absolute: true)
