771cc20176839c733f30fba2008c11ce96e74e11
Igor Artamonov <igor@artamonov.ru>
2013-01-27 21:40:01 +0700

    v0.11

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,7 +54,7 @@ grails.project.dependency.resolution = {
     plugins {
         provided ':webxml:1.4.1'
 
-        build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
             export = false
         }
     }
