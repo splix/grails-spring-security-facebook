9f8ad250e424740e0878928e33ef92ad842418f6
Igor Artamonov <igor@artamonov.ru>
2013-04-20 16:10:20 +0700

    upgrade to Grails 2.1.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,6 +54,8 @@ grails.project.dependency.resolution = {
     plugins {
         provided ':webxml:1.4.1'
 
+        compile ':spring-security-core:1.2.7.2'
+
         build(':release:2.2.0', ':rest-client-builder:1.0.3') {
             export = false
         }
