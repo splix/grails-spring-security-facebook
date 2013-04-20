0b77f2ffbbc827a855a4c1441b1d5da259ed670a
Igor Artamonov <igor@artamonov.ru>
2013-04-20 18:10:37 +0700

    unit tests

--- test/unit/com/the6hours/grails/springsecurity/facebook/FacebookAuthProviderSpec.groovy
+++ test/unit/com/the6hours/grails/springsecurity/facebook/FacebookAuthProviderSpec.groovy
@@ -1,5 +1,6 @@
 package com.the6hours.grails.springsecurity.facebook
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import spock.lang.Specification
 
 /**
@@ -36,4 +37,55 @@ class FacebookAuthProviderSpec extends Specification {
                 expireAt: new Date()
         )
     }
+
+    def "Create a new user"() {
+        setup:
+        FacebookAuthDao dao = Mock(FacebookAuthDao)
+        FacebookAuthUtils utils = Mock(FacebookAuthUtils)
+        FacebookAuthProvider provider = new FacebookAuthProvider(
+                facebookAuthDao: dao,
+                facebookAuthUtils: utils
+        )
+        FacebookAuthToken token = new FacebookAuthToken(
+                uid: 1
+        )
+        TestFacebookUser user = new TestFacebookUser(
+                uid: 1
+        )
+        TestAppUser appUser = new TestAppUser()
+        FacebookAccessToken accessToken = new FacebookAccessToken(
+                accessToken: 'test',
+                expireAt: new Date()
+        )
+        when:
+        provider.authenticate(token)
+        then:
+        1 * dao.findUser(1) >> null
+        1 * utils.getAccessToken(_, _) >> accessToken //load token before creation
+        1 * dao.create(token) >> user //create user
+        1 * dao.hasValidToken(user) >> true
+        1 * dao.getAppUser(user) >> appUser
+        1 * dao.getPrincipal(appUser) >> "hello!"
+        1 * dao.getRoles(_) >> []
+    }
+
+    def "Don't create a new user when disabled"() {
+        setup:
+        FacebookAuthDao dao = Mock(FacebookAuthDao)
+        FacebookAuthUtils utils = Mock(FacebookAuthUtils)
+        FacebookAuthProvider provider = new FacebookAuthProvider(
+                facebookAuthDao: dao,
+                facebookAuthUtils: utils,
+                createNew: false
+        )
+        FacebookAuthToken token = new FacebookAuthToken(
+                uid: 1
+        )
+        when:
+        provider.authenticate(token)
+        then:
+        1 * dao.findUser(1) >> null
+        thrown(UsernameNotFoundException)
+    }
+
 }
